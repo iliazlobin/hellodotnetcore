d9b33eb8dcf279c0196b2656df499b679fdc6e3f
Ilia Zlobin <iliazlobin91@gmail.com>
2020-11-30 17:26:16 +0300

    Provide some dev code

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace hellodotnetcore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World 2!");
+                    await context.Response.WriteAsync("Hello World develop!");
                 });
             });
         }
